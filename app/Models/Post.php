c4ca96d77ec37d58a6b37dc70e3976ef423fd0b7
Santiago <santiagosl314@hotmail.com>
2022-07-18 17:58:16 +0200

    Inicios del proyecto

---
+++ app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+
+    // Relacion uno a muchos inversa
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relacion muchos a mcuhos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // Relacion uno a uno polimorfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        User::create([
+            'name' => 'Santiago Sanchez Ruiz',
+            'email' => 'santiagosl314@hotmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        
+        User::factory(99)->create();
+    }
+}
