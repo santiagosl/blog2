87e76c9ac9fc5c0831bbad16cb2b62478759024f
Santiago <santiagosl314@hotmail.com>
2022-07-31 10:32:48 +0200

    form colletive

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Crear nueva categoria</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+           
+        </div>
+    </div>
+@stop
+
