97f3e638994fd33565c6d9dce137a7173955208e
Santiago <santiagosl314@hotmail.com>
2022-08-03 13:05:33 +0200

    eliminar posts y observers

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -134,6 +134,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin.posts.index', $post)->with('info' , 'El post se elimino con exito');
     }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -10,6 +10,16 @@
 @stop
 
 @section('content')
+@if (session('info'))
+
+    <div class="alert alert-success">
+
+        <strong>{{session('info')}}</strong>
+
+    </div>
+    
+@endif
+
     @livewire('admin.posts-index')
 @stop
 
